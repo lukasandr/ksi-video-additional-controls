302d69442d76f9a5a25874bc1ff28d2c2660e7a5
Lukas Andriejunas <lukas.andriejunas@gmail.com>
2020-09-02 13:29:43 +0300

    Add files via upload

---
+++ ksi-video-additional-controls.js
@@ -0,0 +1,26 @@
+var thisVideo = document.getElementById("myVideo_html5_api");
+  document.onkeydown = function(event) {
+      switch (event.keyCode) {
+         case 37:
+              event.preventDefault();
+              vid_currentTime = thisVideo.currentTime;
+              thisVideo.currentTime = vid_currentTime - 5;
+            break;
+         
+         case 39:
+              event.preventDefault();
+              vid_currentTime = thisVideo.currentTime;
+              thisVideo.currentTime = vid_currentTime + 5;
+            break;
+
+         case 32:
+         		event.preventDefault();
+         		if (thisVideo.paused) {
+         			thisVideo.play();
+         		} else {
+         			thisVideo.pause();
+         		}
+         	break;
+         
+      }
+  };
